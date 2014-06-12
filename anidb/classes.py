0217221399c2d8ffb4ab1c294e04f2245d3b2d2e
ndminh92 <ndminh92@gmail.com>
2014-06-12 12:51:31 -0400

    Working on crc32 check for files

--- anidb/classes.py
+++ anidb/classes.py
@@ -1,6 +1,7 @@
 import sys
 import zlib
 import os
+import datetime
 
 QUALITY = ['unknown', 'TV', 'DVD', 'BD']
 
@@ -11,10 +12,9 @@ class Series(object):
         """Set default values for a blank series"""
         self.location = location
         self.episode_number = episode_number
-        self.episode_list = [None for i in range (episode_number)]
+        self.episode_list = [None for i in range(episode_number)]
         #quality = QUALITY[0] 
-        
-        
+    
     def get_episode(self, number):
         return self.episode_list[number]
         
@@ -37,18 +37,71 @@ class Series(object):
         else:
             self.episode_list[episode.number] = episode
     
+    
+    
 class Episode(object):
-    """An anime episode. Contain information about just one episode"""
+    """An anime episode. Contain information about just one episode
+    Attribute:
+        number      : episode number
+        filename    : system file name. may include whitespace
+        filepath    : system file path
+        crc32       : crc32 hash of the file
+        last_checked: date of the last crc32 check
+        
+    Methods:
+        __init__    : initialize new Episode
+        print_name  : print the filename
+    """
     
     def __init__(self, number=0, crc32="", filename="", filepath=""):
         self.number = number
         self.crc32 = crc32
         self.filename = filename
         self.filepath = filepath
-   
-    def print_filename(self):
+        self.crc_match = True
+        self.last_checked = datetime.date(1,1,1)
+        
+    def print_name(self):
         print self.filename
+       
+    def right_path(filepath):
+        """Check if the path recorded corresponds to file"""
+        return os.isfile(filepath)
     
+    def get_crc32():
+        """Return CRC32 hash of file
+        If no file found return "" 
+        """
+        if os.path.isfile(self.filepath):
+            myfile = open(self.filepath, 'rb')
+            data = myfile.read()
+            myfile.close()
+            crc = zlib.crc32(data)
+            return "%X" %(crc & 0xFFFFFFFF) #Convert to unsigned hex
+        else: # No file found
+            return ""
+        
+    def update_crc32():
+        """Update file crc32, setting crc_match to False if wrong
+        Return True if successful, False if mismatch
+        First time check update crc and check in file name
+        """
+        crc_check = self.get_crc32()
+        if self.crc32 == "": #First time
+            self.crc32 = crc_check
+            if crc_check in self.filename:
+                crc_match = True
+                return True
+            else:
+                crc_match = False
+                return False        
+        elif crc_check == self.crc32: #Second time, crc match
+            self.lastchecked = self.lastchecked.today()
+            return True
+        else:
+            self.crc32 = crc_check
+            return False
+            
 """
 For checking directories use
     os.listdir
--- tests/anidb_test.py
+++ tests/anidb_test.py
@@ -7,7 +7,7 @@ def test_basic():
 
 def test_filename():    
     ep1 = classes.Episode(1, "A34F324CD", "[FFF] Black Bullet 01 [A34F324CD].mkv", "D:\Download\[FFF] Black Bullet 01 [A34F324CD].mkv")
-    ep1.print_filename()
+    ep1.print_name()
     assert_equal(ep1.filename,"[FFF] Black Bullet 01 [A34F324CD].mkv") 
     
 def test_add_episode():
@@ -16,4 +16,4 @@ def test_add_episode():
     series1.add_episode(ep1)
     series1.print_episode_list()
     assert_equal(series1.get_episode(1),ep1)
-    series1.get_episode(1).print_filename()
+    series1.get_episode(1).print_name()
